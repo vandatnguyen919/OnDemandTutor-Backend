9a53112cbbe82375def24999e8bf128468082006
vandatnguyen919 <vandatnguyen919@gmail.com>
2024-05-15 19:46:24 +0700

    first commit

---
+++ src/main/java/com/mytutor/MyTutorApplication.java
@@ -0,0 +1,13 @@
+package com.mytutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytutor/MyTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
