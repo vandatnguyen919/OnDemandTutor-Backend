ecb53376a7bb2836ff7cccf972ea9987cbee89ee
vandatnguyen919 <vandatnguyen919@gmail.com>
2024-06-08 15:29:02 +0700

    create & check payment

--- src/main/java/com/mytutor/controllers/PaymentController.java
+++ src/main/java/com/mytutor/controllers/PaymentController.java
@@ -1,13 +1,13 @@
 package com.mytutor.controllers;
 
+import com.mytutor.dto.payment.RequestPaymentDto;
 import com.mytutor.services.PaymentService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.security.Principal;
 
 @RestController
@@ -18,13 +18,21 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @GetMapping("/create-payment")
-    public ResponseEntity<?> createPayment() {
-        return null;
+    public ResponseEntity<?> createPayment(
+            Principal principal,
+            HttpServletRequest req,
+            @RequestBody RequestPaymentDto requestPaymentDto
+    ) {
+        return paymentService.createPayment(principal, req, requestPaymentDto);
     }
 
-    @GetMapping("/check-payment")
-    public ResponseEntity<?> checkPayment() {
-        return null;
+    @GetMapping("/check-payment/vnpay")
+    public ResponseEntity<?> checkPayment(
+            HttpServletRequest req,
+            @RequestParam(name = "vnp_TxnRef") String vnp_TxnRef,
+            @RequestParam(name = "vnp_PayDate") String vnp_TransDate
+    ) throws IOException {
+        return paymentService.checkVNPayPayment(req, vnp_TxnRef, vnp_TransDate);
     }
 
 //    @GetMapping("/refund-payment")
