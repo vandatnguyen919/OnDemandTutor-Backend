964041352076df3967ae9e1b7c583bca6bc22af6
vandatnguyen919 <vandatnguyen919@gmail.com>
2024-06-20 13:42:28 +0700

    fix quest status enum

--- src/main/java/com/mytutor/entities/Question.java
+++ src/main/java/com/mytutor/entities/Question.java
@@ -5,12 +5,8 @@
 package com.mytutor.entities;
 
 import com.mytutor.constants.QuestionStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -40,6 +36,7 @@ public class Question {
 
     private String questionUrl;
 
+    @Enumerated(EnumType.STRING)
     private QuestionStatus status;
 
     @ManyToOne
