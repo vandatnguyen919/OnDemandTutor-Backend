35399dd7bef34abe04870c60e4c2e7cef9b57a92
vandatnguyen919 <vandatnguyen919@gmail.com>
2024-07-17 11:57:26 +0700

    set to vietnamese time zone

--- src/main/java/com/mytutor/MyTutorApplication.java
+++ src/main/java/com/mytutor/MyTutorApplication.java
@@ -1,8 +1,11 @@
 package com.mytutor;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,9 @@ public class MyTutorApplication {
 		SpringApplication.run(MyTutorApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		// Setting default time zone to Vietnamese time zone
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 }
